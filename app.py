8f49f3eafcbe1a1798920cc8ed5852ffadc4e3c3
johncodziii <jcbizar@cec.edu.ph>
2025-04-29 10:18:02 +0800

    third commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello World! From FLASK"
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
