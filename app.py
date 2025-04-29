fe13d385beb00fcfcd6d32717cb4d0658ea34a1f
johncodziii <jcbizar@cec.edu.ph>
2025-04-29 10:25:26 +0800

    fourth commit

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello World! From FLASK"
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
